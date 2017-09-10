f2ec037d893f6af66bb8490ca77e791d0280f93f
Flavien Moutawe <flavien.moutawe@modisfrance.fr>
2017-09-10 18:08:53 +0200

    - Modification de la présentation de la programmation orienté objet - Modification de la présentation de composer - Ajout de la présentation  de Symfony par ses composants

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,24 @@
-
     // More info about config & dependencies:
     // - https://github.com/hakimel/reveal.js#configuration
     // - https://github.com/hakimel/reveal.js#dependencies
     Reveal.initialize({
+        controls: true,
+        progress: true,
+        keyboard: true,
+        overview: true,
+        center: true,
+        touch: true,
+        history: true,
+        fragments: true,
+        transition: 'convex',
         dependencies: [
-            { src: '../assets/plugin/markdown/marked.js' },
-            { src: '../assets/plugin/markdown/markdown.js' },
-            { src: '../assets/plugin/notes/notes.js', async: true },
-            { src: '../assets/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+            {src: '../assets/plugin/markdown/marked.js'},
+            {src: '../assets/plugin/markdown/markdown.js'},
+            {src: '../assets/plugin/notes/notes.js', async: true},
+            {
+                src: '../assets/plugin/highlight/highlight.js', async: true, callback: function () {
+                hljs.initHighlightingOnLoad();
+            }
+            }
         ]
     });
